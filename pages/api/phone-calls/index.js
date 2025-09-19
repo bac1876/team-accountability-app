1b03039b7b54270357f976832314ab7fa62e8877
Brian <brian@osirs.co>
2025-09-19 09:44:46 -0600

    Force redeployment of phone-calls API

--- pages/api/phone-calls/index.js
+++ pages/api/phone-calls/index.js
@@ -120,4 +120,4 @@ export default async function handler(req, res) {
       res.setHeader('Allow', ['GET', 'POST', 'PUT'])
       return res.status(405).json({ error: `Method ${method} not allowed` })
   }
-}
+}// Force deployment
